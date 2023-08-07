87495398ee7be0fe9cd01cf02c6a7598eda91e39
Nugzar <nskhiereli@gmail.com>
2023-08-07 19:24:47 +0400

    chore

--- src/MolareServiceProvider.php
+++ src/MolareServiceProvider.php
@@ -8,14 +8,11 @@ class MolareServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     public function register()
     {
-        // $this->app->bind('molare', function ($app) {
-        //     return new Molare();
-        // });
+
     }
 }
--- src/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use NUG01\Molare\Http\Controllers\SubscriptionController;
-use Illuminate\Support\Facades\Route;
-use NUG01\Molare\Http\Controllers\SubscriptionController\index;
-
-Route::get('molare', SubscriptionController::class . '@index');
